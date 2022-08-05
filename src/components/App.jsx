f4b566197c24004e4255940ee1308303230668bf
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-05 22:14:39 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { Profile } from './Profile/Profile';
+import { FriendList } from './FriendList/FriendList';
+import { Statistics } from './Statistics/Statistics';
+import { History } from './TransactionHistory/TransactionHistory';
+console.log({ Statistics });
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile />
+      <Statistics />
+      <FriendList />
+      <History />
     </div>
   );
 };
