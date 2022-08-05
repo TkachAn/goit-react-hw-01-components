163b2a86302f25a0b0254a944ed211111e888715
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-05 22:13:20 +0300

    Create StatList.styled.js

---
+++ src/components/StatList/StatList.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+export const StatInfo = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+// import { StatInfo } from './StatList.styled';
