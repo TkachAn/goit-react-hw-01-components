54db03d1ada35dcb0081714f55945112136c81c1
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-06 19:55:31 +0300

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,28 +1,44 @@
 import user from '../user.json';
-
+import PropTypes from 'prop-types';
+import { ProfileS } from './Profile.styled';
+import { Stats } from './Profile.styled';
+import { StatsEl } from './Profile.styled';
 export const Profile = () => {
   return (
-    <div className="profile">
+    <ProfileS className="profile">
       <div className="description">
         <img src={user.avatar} alt={user.username} className="avatar" />
         <p className="name">{user.username}</p>
-        <p className="tag">{user.tag}</p>
+        <p className="tag">@{user.tag}</p>
         <p className="location">{user.location}</p>
-        <ul className="stats">
-          <li>
-            <span className="label">Followers</span>
-            <span className="quantity">{user.stats.followers}</span>
-          </li>
-          <li>
-            <span className="label">Views</span>
-            <span className="quantity">{user.stats.views}</span>
-          </li>
-          <li>
-            <span className="label">Likes</span>
-            <span className="quantity">{user.stats.likes}</span>
-          </li>
-        </ul>
       </div>
-    </div>
+      <Stats className="stats">
+        <StatsEl>
+          <span className="label">Followers</span>
+          <span className="quantity">{user.stats.followers}</span>
+        </StatsEl>
+        <StatsEl>
+          <span className="label">Views</span>
+          <span className="quantity">{user.stats.views}</span>
+        </StatsEl>
+        <StatsEl>
+          <span className="label">Likes</span>
+          <span className="quantity">{user.stats.likes}</span>
+        </StatsEl>
+      </Stats>
+    </ProfileS>
   );
+};
+Profile.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired,
+  }), //.isRequired,
 };
