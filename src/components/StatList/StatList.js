0dd1a6f9963d139e25cc3e45ddc9147af5dadda0
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-07 11:40:57 +0300

    Update StatList.js

--- src/components/StatList/StatList.js
+++ src/components/StatList/StatList.js
@@ -1,15 +1,19 @@
-//import { Item } from 'components/StatItem/StatItem';
-//import { StatInfo } from './StatList.styled';
+import PropTypes from 'prop-types';
 import data from '../data.json';
 export const StatList = () => {
   return (
     <>
       {data.map(item => (
         <li key={item.id}>
-          <span className="label">{item.label}</span>:{' . . . . . '}
-          <span className="percentage">{item.percentage}</span>%.
+          <span className="label">{item.label}</span>
+          <span className="percentage">{item.percentage}%.</span>
         </li>
       ))}
     </>
   );
+};
+
+data.propTypes = {
+  label: PropTypes.string.isRequired,
+  percentage: PropTypes.string.isRequired,
 };
