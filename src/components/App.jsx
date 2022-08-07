67935248e62c8d1450dac8753c218dc041d14dab
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-07 14:52:15 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
 import { Profile } from './Profile/Profile';
-
+import user from './user.json';
+import data from './data.json';
 import { FriendList } from './FriendList/FriendList';
 import { Statistics } from './Statistics/Statistics';
 import { History } from './TransactionHistory/TransactionHistory';
 export const App = () => {
   return (
     <div>
-      <Profile />
-      <Statistics />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data} />
       <FriendList />
 
       <History />
