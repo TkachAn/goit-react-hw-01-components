09c06a9cd183e7600d33ab6f5f6a36ab31157dbe
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-07 21:31:12 +0300

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { ProfileS } from './Profile.styled';
 import { Stats } from './Profile.styled';
 import { StatsEl } from './Profile.styled';
+import css from './Profile.module.css';
 export const Profile = ({
   avatar,
   username,
@@ -11,25 +12,25 @@ export const Profile = ({
   stats: { followers, views, likes },
 }) => {
   return (
-    <ProfileS className="profile">
-      <div className="description">
-        <img src={avatar} alt={username} className="avatar" />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+    <ProfileS className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt={username} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
-      <Stats className="stats">
+      <Stats className={css.stats}>
         <StatsEl>
-          <span className="label">Followers</span>
-          <span className="quantity">{followers}</span>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
         </StatsEl>
         <StatsEl>
-          <span className="label">Views</span>
-          <span className="quantity">{views}</span>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
         </StatsEl>
         <StatsEl>
-          <span className="label">Likes</span>
-          <span className="quantity">{likes}</span>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
         </StatsEl>
       </Stats>
     </ProfileS>
