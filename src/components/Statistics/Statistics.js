b081b9b3deee9ab02458a9233a7b576ddf84f164
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-07 14:52:05 +0300

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,30 @@
+//import PropTypes, { array } from 'prop-types';
 import { StatList } from '../StatList/StatList';
-export const Statistics = () => {
+//import data from './data.json';
+export function Statistics({ stats }) {
+  //console.log('stats', stats);
+  //console.log('stats.id', stats.id);
   return (
     <section className="statistics">
       <h2 className="title">Upload stats</h2>
       <ul className="stat-list">
-        <StatList />
+        {stats.map(stat => {
+          return (
+            <li key={stat.id}>
+              <StatList
+                id={stat.id}
+                label={stat.label}
+                percentage={stat.percentage}
+              />
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
-};
+}
+
+// array.propTypes = {
+//   label: PropTypes.string.isRequired,
+//   percentage: PropTypes.number.isRequired,
+// };
