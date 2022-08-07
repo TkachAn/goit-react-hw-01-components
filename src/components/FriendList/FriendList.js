769ab1998d3a60b081fc8a09ced97d17f554dc95
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-07 16:53:34 +0300

    Update FriendList.js

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,9 +1,19 @@
 import { FriendItem } from '../FriendItem/FriendItem';
 
-export const FriendList = () => {
+export function FriendList({ friend }) {
   return (
     <ul className="friend-list">
-      <FriendItem />
+      {friend.map(item => {
+        return (
+          <li key={item.id} className="item">
+            <FriendItem
+              isOnline={item.isOnline}
+              avatar={item.avatar}
+              name={item.name}
+            />
+          </li>
+        );
+      })}
     </ul>
   );
-};
+}
