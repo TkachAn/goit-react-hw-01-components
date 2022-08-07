9fbab52780c3d7f19891af993022493a69e0698b
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-07 22:08:59 +0300

    Update FriendItem.styled.js

--- src/components/FriendItem/FriendItem.styled.js
+++ src/components/FriendItem/FriendItem.styled.js
@@ -1,12 +1,1 @@
-import styled from 'styled-components';
-export const Online = styled.span`
-  width: 25px;
-  height: 25px;
-  background-color: isOnline?green:red;
-  border-radius: 50%;
-  ${'' /* display: flex; */}
-  ${'' /* align-items: center; */}
-  ${'' /* gap: 8px; */}
-  ${'' /* max-width: 480px; */}
-  ${'' /* text-align: center; */}
-`;
+
