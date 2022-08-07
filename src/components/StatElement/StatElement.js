e2f418ee998bcccccd8c9a246a086f55b7a6a8fc
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-07 21:31:02 +0300

    Update StatElement.js

--- src/components/StatElement/StatElement.js
+++ src/components/StatElement/StatElement.js
@@ -1,8 +1,16 @@
+// import PropTypes from 'prop-types';
+import css from './StatElement.module.css';
 export const StatElement = ({ label, percentage }) => {
   return (
     <>
-      <span className="label">{label}</span>
-      <span className="percentage">{percentage}%.</span>
+      <span className={css.label}>{label}</span>
+      <span className={css.percentage}>{percentage}%.</span>
     </>
   );
 };
+// StatElement.propTypes = {
+//   data: PropTypes.shape({
+//     label: PropTypes.string.isRequired,
+//     percentage: PropTypes.number.isRequired,
+//   }),
+// };
