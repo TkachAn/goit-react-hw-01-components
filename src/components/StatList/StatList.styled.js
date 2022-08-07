87480c3004caf94a489c812d9ae54859a72d56bb
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-07 15:09:29 +0300

    Delete StatList.styled.js

--- src/components/StatList/StatList.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-export const StatInfo = styled.li`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-`;
-// import { StatInfo } from './StatList.styled';
