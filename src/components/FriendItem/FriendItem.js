4e8bf9a3242710c9ad3172efcb94f2c23561ac1e
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-07 11:41:12 +0300

    Update FriendItem.js

--- src/components/FriendItem/FriendItem.js
+++ src/components/FriendItem/FriendItem.js
@@ -1,4 +1,6 @@
+import PropTypes from 'prop-types';
 import friends from '../friends.json';
+// import { Online } from './FriendItem.styled';
 export const FriendItem = () => {
   return (
     <>
@@ -10,11 +12,16 @@ export const FriendItem = () => {
             src={item.avatar}
             alt={item.name}
             width={'48'}
-            height={'48'}
           />
           <p className="name">{item.name}</p>
         </li>
       ))}
     </>
   );
+};
+friends.propTypes = {
+  id: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
 };
