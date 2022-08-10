e30a09914eabae7fb04bdf174447ad114fe2eda9
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-10 11:15:09 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics stats={data} />
+      <Statistics title="Upload stats" stats={data} />
       <FriendList friend={friends} />
       <History trans={transactions} />
     </div>
