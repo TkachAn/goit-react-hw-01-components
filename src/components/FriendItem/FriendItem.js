cd0c3797f18f6d5f692bae3bb93ad11f02413881
TkachAn <4ndrey.tkachenko@gmail.com>
2022-08-10 13:32:03 +0300

    Update FriendItem.js

--- src/components/FriendItem/FriendItem.js
+++ src/components/FriendItem/FriendItem.js
@@ -3,19 +3,13 @@ import css from './FriendItem.module.css';
 export const FriendItem = ({ isOnline, avatar, name }) => {
   return (
     <>
-      {isOnline ? (
-        <span className={css.status}>{isOnline}</span>
-      ) : (
-        <span className={css.status_}>{isOnline}</span>
-      )}
-
+      <span className={isOnline ? css.status : css.status_}>{isOnline}</span>
       <img className="avatar" src={avatar} alt={name} width={'48'} />
       <p className="name">{name}</p>
     </>
   );
 };
 FriendItem.propTypes = {
-  id: PropTypes.number,
   avatar: PropTypes.string.isRequired,
   isOnline: PropTypes.bool.isRequired,
   name: PropTypes.string.isRequired,
